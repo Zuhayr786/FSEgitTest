d7ef3081ddd90b0df5b0e9b98aa4fe3c8e1a2f62
Jimmy Smith <jimmysmith@gcu.ac.uk>
2020-11-19 10:15:08 +0000

    Person class first commit

---
+++ person.java
@@ -0,0 +1,14 @@
+public class person
+{
+private String firstName;
+private String othername;
+Private String surename;
+
+    public person()
+    {
+     firstName = "";
+     othername = "";
+     surename = "";
+    }
+
+}
