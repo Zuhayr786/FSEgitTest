d76006c3a1e4633c40f5b3c87d86a4346ee239a2
Jimmy Smith <jimmysmith@gcu.ac.uk>
2020-11-19 21:54:22 +0000

    Person class added getters and setters second commit

--- person.java
+++ person.java
@@ -11,4 +11,39 @@ Private String surename;
      surename = "";
     }
 
+    public String getFirstName()
+    {
+     return firstName;
+    }
+
+    public String getothername()
+    {
+     return othername;
+    }
+
+    public String getsurename()
+    {
+     return surename;
+    }
+
+
+    
+
+    public void setFirstName(String FIRSTNAME)
+    {
+     firstName = FIRSTNAME;
+    }
+
+    public void setothername(String OTHERNAME)
+    {
+     othername = OTHERNAME;
+    }
+
+    public void setsurename(String SURENAME)
+    {
+     surename = SURENAME;
+    }
+
+
+
 }
